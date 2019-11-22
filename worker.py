e1d99c33f56d18576137e9223e46a96a5c5a8b1c
leva-14 <lev201611@gmail.com>
2019-11-22 17:19:47 +0600

    classing commit

--- worker.py
+++ worker.py
@@ -1,1 +1,24 @@
 class Programmist:
+
+    profession = "Programmist"
+    age = "14"
+    height = "176"
+    studying = "iStudy"
+    eye_color = "blue"
+    hair_color = "blond"
+    date = "26.01.2005"
+    email = "lev201611@gmail.com"
+    nick = "Levasik"
+    gender = "Men"
+
+    def start(self):
+        print("Просыпаюсь")
+
+    def eating(self):
+        print("Кушаю")
+
+    def school(self):
+        print("Иду в школу")
+
+    def home(self):
+        print("Возвращаюсь домой")
