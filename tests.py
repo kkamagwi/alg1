bef86563242665fbd40843be94082e8a81b8cd11
Katya <laptop@Laptops-MacBook-Air.local>
2019-11-22 17:45:22 +0600

    create tests self

--- tests.py
+++ tests.py
@@ -1,6 +1,9 @@
 import unittest
+from alg1.worker import Programmist
 
 
-class TestWorker:
+class TestWorker(unittest.TestCase):
 
     def test_first(self):
+        georgii = Programmist()
+        self.assertEqual(georgii.email, "lev201611@gmail.com")
