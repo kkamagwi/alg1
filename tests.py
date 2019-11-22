89a12ea0a6fc8dcb67a84771b40bba3c0d2e254c
leva-14 <lev201611@gmail.com>
2019-11-22 17:51:22 +0600

    semki

--- tests.py
+++ tests.py
@@ -7,3 +7,7 @@ class TestWorker(unittest.TestCase):
     def test_first(self):
         georgii = Programmist()
         self.assertEqual(georgii.email, "lev201611@gmail.com")
+
+    def test_isupper(self):
+        georgii = Programmist()
+        self.assertEqual(georgii.nick, "Levasik")
